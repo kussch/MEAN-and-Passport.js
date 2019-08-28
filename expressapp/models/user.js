86f56fbe3a2b565ed80a0b600c136c7cfa2e0788
Roman Kushch <41487922+kussch@users.noreply.github.com>
2019-08-28 19:59:08 +0300

    Update user.js

--- expressapp/models/user.js
+++ expressapp/models/user.js
@@ -3,17 +3,17 @@ var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt');
 
 var schema = new Schema({
-    email : {type:String, require:true},
-    username: {type:String, require:true},
-    password: {type:String, require:true},
-    creation_dt: {type:Date, require:true}
+    email: {type: String, require: true},
+    username: {type: String, require: true},
+    password: {type: String, require: true},
+    creation_dt: {type: Date, require: true}
 });
 
-schema.statics.hashPassword = function hashPassword(password){
+schema.statics.hashPassword = function hashPassword(password) {
     return bcrypt.hashSync(password, 10);
 }
 
-schema.methods.isValid = function(hashedpassword){
+schema.methods.isValid = function(hashedpassword) {
     return  bcrypt.compareSync(hashedpassword, this.password);
 }
 
